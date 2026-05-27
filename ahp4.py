2db6700bdf6b7656b002402cbfedf6d2f0239814
L-03-ZX <zixin9212@gmail.com>
2026-05-27 21:14:11 +0800

    Update ahp4.py

--- ahp4.py
+++ ahp4.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import base64
-import matplotlib
 
 # --- AHP Mathematical Constants ---
 RI_DICT = {1: 0.00, 2: 0.00, 3: 0.58, 4: 0.90, 5: 1.12, 6: 1.24, 7: 1.32, 8: 1.41, 9: 1.45, 10: 1.49}
